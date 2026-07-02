eeb34e2e2f02ee20749188b67c904dc420ac2793
rahul yadav <yadavrahul784ej@gmail.com>
2026-07-02 23:30:57 +0530

    feat: setup Tailwind CSS and React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <div>
-      <h1>🚚 RouteIQ</h1>
-      <p>Project setup successful!</p>
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <h1 className="text-4xl font-bold text-blue-600">
+          🚚 RouteIQ
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Tailwind CSS is working successfully!
+        </p>
+      </div>
     </div>
   );
 }
