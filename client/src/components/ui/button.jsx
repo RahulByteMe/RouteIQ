e95c0a8aa8f9b371dc4db8ead0a33c02a32b8941
rahul yadav <yadavrahul784ej@gmail.com>
2026-07-05 08:58:33 +0530

    feat: create reusable Button component

---
+++ client/src/components/ui/button.jsx
@@ -0,0 +1,11 @@
+
+function Button({ text }){
+
+    return (
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            {text}
+        </button>
+    )
+}
+
+export default Button
