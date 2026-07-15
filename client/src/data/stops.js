de3521eb26a7c40aa26e666b13f00f62a138fa98
rahul yadav <yadavrahul784ej@gmail.com>
2026-07-15 11:47:43 +0530

    feat: implement dynamic delivery stop creation

---
+++ client/src/data/stops.js
@@ -0,0 +1,28 @@
+const stops = [
+    {
+        id: 1,
+        name: "Stop 1",
+        position: [31.4685, 76.2708]
+    },
+    {
+        id: 2,
+        name: "Stop 2",
+        position: [31.4690, 76.2710]
+    },
+    {
+        id: 3,
+        name: "Stop 3",
+        position: [31.4700, 76.2720]
+    }
+];
+const newStop = {
+
+    id:4,
+
+    name:"Rahul House",
+
+    position:[31.4715,76.2730]
+
+}
+export default stops;
+export { newStop };
--- client/src/pages/MapDemo.jsx
+++ client/src/pages/MapDemo.jsx
@@ -1,8 +1,43 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import initialStops from "../data/stops";
 
 function MapDemo() {
+    const [name, setName] = useState("");
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
+    const [stops, setStops] = useState(initialStops);
+    console.log(stops);
+
+    function handleAddstop(){
+         console.log("Button clicked");
+    const newstop = {
+        id: stops.length+1,
+        name: name,
+        position: [Number(latitude), Number(longitude)]
+    };
+    setStops([...stops, newstop]);
+    console.log(newstop);
+
+     if (!name || !latitude || !longitude) {
+                alert("Please fill all fields");
+                return;
+            }
+
+    }
+   
 
     return (
+        
+        <>
+        <input placeholder="Stop Name" value={name} onChange={(e)=>setName(e.target.value)} />
+        <input placeholder="Latitude"  value={latitude} onChange={(e)=>setLatitude(e.target.value)} />
+        <input placeholder="Longitude"  value={longitude} onChange={(e)=>setLongitude(e.target.value)} />
+        
+
+        <button onClick={handleAddstop}>
+                Add Stop
+            </button>
 
         <MapContainer
             center={[31.4685, 76.2708]}
@@ -10,20 +45,25 @@ function MapDemo() {
             scrollWheelZoom={false}
             style={{ height: "100vh", width: "100%" }}
         >
+
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; OpenStreetMap contributors'
             />
-            <Marker position={[31.4685, 76.2708]}>
-                <Popup>
-                    🚚 RouteIQ Depot
-                </Popup>
-            </Marker>
+            {stops.map((stop) => (
+                <Marker key={stop.id} position={stop.position}>
+                    <Popup>
+                        {stop.name}
+                    </Popup>
+                </Marker>
+            ))}
 
         </MapContainer>
+        </>
 
     )
 
 }
+
 
 export default MapDemo;
